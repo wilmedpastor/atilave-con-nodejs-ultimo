7e65053a188f5fdaa517acdf4bd426f7fc3509d1
WILLIAM MEDINA VARGAS <91066281+wilmedpastor@users.noreply.github.com>
2021-12-09 17:24:53 -0500

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongodb: {
-        URI: 'mongodb+srv://wilmedpastor:8135@diplomadounab.rggju.mongodb.net/atilave_ultimo_nodejs?retryWrites=true&w=majority'
+        URI: 'mongodb'
     }
 };
